e16ea89962d2016a0857232498ef114e9d88cd38
beenj <beenj@DESKTOP-IHPIJC4>
2025-04-17 16:57:03 -0500

    Add project files.

---
+++ Door.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Locked_Door
+{
+    enum DoorState
+    {
+        LOCKED,
+        CLOSED,
+        OPEN
+    }
+
+    internal class Door
+    {
+
+        int Passcode;
+
+        DoorState doorstate;
+
+        public Door(int Passcode)
+        {
+            doorstate = DoorState.LOCKED;
+            this.Passcode = Passcode;
+        }
+
+
+        public void Run()
+        {
+
+
+
+            while (true)
+            {
+                Console.WriteLine("What would you like to do with the door?");
+
+                if (doorstate == DoorState.LOCKED)
+                {
+                    Console.WriteLine("The door is locked and requires the passcode to open. Would you like to change the passcode? ");
+                    string Response = (Console.ReadLine());
+                    Response.ToLower();
+
+                    if (Response == "yes")
+                    {
+                        while (true)
+                        {
+                            //Console.WriteLine("What is the current password? ");
+
+                            //int GuessInt = Convert.ToInt16(Console.ReadLine());
+
+                            //if (GuessInt == Passcode)
+                            //{
+
+                            //}
+                            //else
+                            //{
+                            //    Console.WriteLine("This is not the current password. Try again. ");
+                            //    continue;
+                            //}
+
+                            //Console.WriteLine("What would you like for the new password to be? ");
+
+                            //Passcode = Convert.ToInt16(Console.ReadLine());
+                            //break;
+                        };
+                    }
+                    else;
+                    while (true)
+                    {
+                        Console.WriteLine("What is the passcode? ");
+                        int Guess = Convert.ToInt16(Console.ReadLine());
+
+                        if (Guess == Passcode)
+                        {
+                            Console.WriteLine("The door is now unlocked. ");
+                            doorstate = DoorState.CLOSED;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("This is not the current password. Try again. ");
+                            continue;
+                        }
+                    }
+
+                }
+                else if (doorstate == DoorState.CLOSED)
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("The door is closed. What would you like to do? ");
+                        string Response = (Console.ReadLine());
+                        Response.ToLower();
+                        if (Response == "open")
+                        {
+                            Console.WriteLine("The door is now open.");
+                            doorstate = DoorState.OPEN;
+                            break;
+                        }
+                        else if (Response == "lock")
+                        {
+                            Console.WriteLine("The door is now locked.");
+                            doorstate = DoorState.LOCKED;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("That is not an option.");
+                            continue;
+                        }
+                    }
+                }
+                else
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("The door is open. Would you like to close it? ");
+                        string Response = (Console.ReadLine());
+                        Response.ToLower();
+                        if (Response == "yes")
+                        {
+                            Console.WriteLine("The door is now closed.");
+                            doorstate = DoorState.CLOSED;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("That is not an option.");
+                            continue;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private GetNewPassword(int GuessInt)
+        {
+            while (true)
+            {
+                Console.WriteLine("What is the current password? ");
+
+                GuessInt = Convert.ToInt16(Console.ReadLine());
+
+                if (GuessInt == Passcode)
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("This is not the current password. Try again. ");
+                    continue;
+                }
+
+                Console.WriteLine("What would you like for the new password to be? ");
+
+                Passcode = Convert.ToInt16(Console.ReadLine());
+                break;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using The_Locked_Door;
+
+
+string PasscodeAsString;
+int Passcode;
+
+while (true)
+{
+    Console.WriteLine("What would you like for the passcode to be?");
+
+    PasscodeAsString = Console.ReadLine();
+    try
+    {
+        Passcode = Convert.ToInt16(PasscodeAsString);
+        break;
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("This is not an acceptable passcode. It must be an integer. ");
+    }
+}
+Door door = new Door(Passcode);
+door.Run();
